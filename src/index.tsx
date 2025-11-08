80559079a3b9884e5bd1498fc4ec5c281f48d2f2
Melotwo <turoka15@gmail.com>
2025-11-08 08:57:02 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// FIX: Corrected path to App.tsx inside the src directory.
-import App from './src/App.tsx';
-import './src/index.css';
+import App from '/src/App.tsx';
+import '/src/index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
