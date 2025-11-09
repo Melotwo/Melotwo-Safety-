9ea886af3342827bf9383087719e19551224d1e6
Melotwo <turoka15@gmail.com>
2025-11-09 15:39:23 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  define: {
+    // This makes the Vercel environment variable available to the client-side code
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  }
 })
