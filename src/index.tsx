63b0b9f38c7440c5c6242074fa49551772ee324b
Melotwo <turoka15@gmail.com>
2025-11-13 20:08:26 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '/src/App.tsx';
+import App from './App.tsx';
 import './index.css';
-import ErrorBoundary from '/src/components/ErrorBoundary.tsx';
+import ErrorBoundary from './components/ErrorBoundary.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
