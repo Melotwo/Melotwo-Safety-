3a48ef113378f7d0917c549ba36bab340f7522d8
Melotwo <turoka15@gmail.com>
2025-11-13 20:12:35 +0200

    Update Productcard.tsx

--- Productcard.tsx
+++ Productcard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ShoppingCart } from 'lucide-react';
-import { PpeProduct } from '/src/types.ts';
+import { PpeProduct } from '../types.ts';
 
 const ProductCard: React.FC<{
   product: PpeProduct;
