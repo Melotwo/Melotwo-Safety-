42183842f4d93eadd10f31a355b4839df9e523e7
Melotwo <turoka15@gmail.com>
2025-11-14 19:26:05 +0200

    Update QuoteModal.tsx

--- QuoteModal.tsx
+++ QuoteModal.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Check, X, Mail, User, Building, Loader2 } from 'lucide-react';
-import { PpeProduct } from '../types.ts';
+import { PpeProduct } from '../types';
 
 const QuoteModal: React.FC<{
   isOpen: boolean;
